3c4b2520549581d8dcca8ba5445a0022e90286b2
letlabs <letlabs.ndj@gmail.com>
2024-10-24 20:34:56 +0100

    Ride starting email notification sending

---
+++ services/notification-service/src/main/java/com/lameute/notification_service/config/EmailConfig.java
@@ -0,0 +1,29 @@
+package com.lameute.notification_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("letlabs4dev@gmail.com");
+        mailSender.setPassword("sawd ydew uqqg tvpc");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
