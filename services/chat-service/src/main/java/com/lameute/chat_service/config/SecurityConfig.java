8d66d40faca96e068ec25702cf7c07a35fe7b2da
letlabs <letlabs.ndj@gmail.com>
2024-10-29 04:03:00 +0100

    security configuration : RequestMatchers authorizations for websocket

--- services/chat-service/src/main/java/com/lameute/chat_service/config/SecurityConfig.java
+++ services/chat-service/src/main/java/com/lameute/chat_service/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(registry ->{
-              registry.requestMatchers("/index", "/ws/**").permitAll();
+              registry.requestMatchers("/index","/ws/**").permitAll();
             });
     return http.build();
   }
