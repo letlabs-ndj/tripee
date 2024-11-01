752b7fdf35e152d121c74a1906b15f00b5e352fc
letlabs <letlabs.ndj@gmail.com>
2024-11-01 13:56:11 +0100

    refactor RideResponse model

---
+++ services/expedition-service/src/main/java/com/lameute/expedition_service/dto/RideResponse.java
@@ -0,0 +1,14 @@
+package com.lameute.expedition_service.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record RideResponse(
+        long id,
+        LocalDate departureDate,
+        LocalTime departureTime,
+        Place departurePlace,
+        Place arrivalPlace,
+        UserResponse user
+) {
+}
